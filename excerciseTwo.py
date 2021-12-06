55d514f3075b85185e0b282a1bbc60267c773728
Dharmendra <30459052+dharmen001@users.noreply.github.com>
2021-12-06 22:52:46 +0530

    Add files via upload

--- excerciseTwo.py
+++ excerciseTwo.py
@@ -3,17 +3,53 @@ from LogFile import logger
 import pandas as pd
 
 
-class ExerciseTwo(object):
+def read_employee(employee):
+    df_employee = pd.read_csv(employee)
+    return df_employee
 
-    @staticmethod
-    def read_manager(manager):
-        df_manager = pd.read_csv(manager)
-        return df_manager
 
-    @staticmethod
-    def read_employee(employee):
-        df_employee = pd.read_csv(employee)
-        return df_employee
+# function to find all employees who directly or indirectly report to a given manager and store the result in `result`
+def find_all_reporting_employees(manager, manager_to_employee_mappings, results):
+    if manager in results:
+        # return the already computed mapping
+        return results.get(manager)
+
+    manager_employees = manager_to_employee_mappings.get(manager)
+
+    for reporting in manager_employees.copy():
+        # find all employees reporting to the current employee
+        employees = find_all_reporting_employees(reporting, manager_to_employee_mappings, results)
+
+        # move those employees to the current manager
+        if employees:
+            manager_employees.update(employees)
+
+    # save the result to avoid re computation and return it
+    results[manager] = manager_employees
+    return manager_employees
+
+
+# Find all employees who directly or indirectly reports to a manager
+def find_employees(employee_to_manager_mappings):
+    # store manager to employee mappings in a new dictionary
+    manager_to_employee_mappings = {}
+
+    # fill the above dictionary with the manager to employee mappings
+    for employee, manager in employee_to_manager_mappings.items():
+        manager_to_employee_mappings.setdefault(employee, set())
+        # don't map an employee with itself
+        if employee != manager:
+            manager_to_employee_mappings.setdefault(manager, set()).add(employee)
+
+    # construct an empty dictionary to store the result
+    result = {}
+
+    # find all reporting employees (direct and indirect) for every manager
+    # and store the result in a dictionary
+    for key in employee_to_manager_mappings.keys():
+        find_all_reporting_employees(key, manager_to_employee_mappings, result)
+
+    return result
 
 
 if __name__ == "__main__":
@@ -23,25 +59,14 @@ if __name__ == "__main__":
     manager_csv = config.get('exercise-2', 'manager_csv')
     employee_csv = config.get('exercise-2', 'employee_csv')
     logger.info("Reading the manager file")
-    manager_read = ExerciseTwo.read_employee(manager_csv)
+    manager_read = read_employee(manager_csv)
     logger.info("Reading the employee file")
-    employee_read = ExerciseTwo.read_employee(employee_csv)
+    employee_read = read_employee(employee_csv)
     # Joining manager and employee ID
     df = pd.merge(manager_read, employee_read, on='Employee_ID', how='inner')
+    child_parent_dict = df.set_index('Employee_ID')['Manager_ID'].to_dict()
+    result = find_employees(child_parent_dict)
     while True:
-        try:
-            employee_id = list(map(int, input("Enter your Employee IDs: ").split(",")))
-            filtered_df = df[df['Employee_ID'].isin(employee_id)]
-            if filtered_df.empty:
-                print("couldn't find the employee ID please enter valid ID")
-            else:
-                print(filtered_df)
-        except Exception as e:
-            print(str(e))
-            continue
-
-
-
-
-
-
+        employee_id = list(map(int, input("Enter your Employee IDs: ").split(",")))
+        res = [result[i] for i in employee_id if i in result]
+        print(res)
